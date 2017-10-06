32c5be8e4bca5c87caf6959b0fb1bbdafa0bb20b
Swayam Mittal <swayam.mittal@yahoo.com>
2017-10-06 14:32:59 +0530

    Create hatch.py

---
+++ mustani/hatch.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import config
+
+class hatch():
+
+    def __init__(self):
+        self.plot_cache = [ ]
+        self.key_value_store = { }
+
+    def add(self, key, value):
+        self.key_value_store[key] = value
+
+    def get(self, key):
+        if (key in self.key_value_store):
+            return self.key_value_store[key]
+        return None
+
+    def extend_plot_cache(self, data):
+        self.plot_cache.extend(data)
+
+    def get_plot_cache(self):
+        return self.plot_cache
